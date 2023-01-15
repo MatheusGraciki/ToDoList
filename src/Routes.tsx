bcb347fa8a89b9030a0455d16ef76c52e7f3e013
Matheus Graciki <85004422+MatheusGraciki@users.noreply.github.com>
2023-01-15 16:43:57 -0300

    Feature/CreateRoutes

---
+++ src/Routes.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+
+// screens
+import CreateTask from "./screens/tabs/createtask";
+import Home from "./screens/tabs/home";
+
+//auth
+import Login from "./screens/auth/login";
+import Registration from "./screens/auth/registration";
+
+export default function Routes() {
+
+    const Stack = createNativeStackNavigator();
+
+    return(
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Registration" component={Registration} />
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="CreateTask" component={CreateTask} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+
