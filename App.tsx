2e0cc9bb7caae7eecbad5cb9dbb1e41abe11e819
Matheus Graciki <85004422+MatheusGraciki@users.noreply.github.com>
2023-01-15 21:07:07 -0300

    Feature/Fix_InvalidSpaces

--- App.tsx
+++ App.tsx
@@ -4,12 +4,7 @@ import Routes from './src/Routes';
 
 export default function App() {
     return (
-
-
         <Routes/>
-
-
-
     );
 }
 
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,10 @@
+// imports
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-
 // screens
 import CreateTask from "./screens/tabs/createtask";
 import Home from "./screens/tabs/home";
-
 //auth
 import Login from "./screens/auth/login";
 import Registration from "./screens/auth/registration";
